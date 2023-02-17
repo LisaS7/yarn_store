aa567af1790472309c1753dbb057fa254c65076b
Lisa <mclisa00@gmail.com>
2023-02-17 14:12:19 +0000

    imported test classes

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,8 @@
 import unittest
 
-# import Test classes from test files
+from tests.colour_test import TestColour
+from tests.manufacturer_test import TestManufacturer
+from tests.yarn_test import TestYarn
 
 if __name__ == "__main__":
     unittest.main()
