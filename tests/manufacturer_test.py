de31dec60c0048447b86e31993fdd9e7352c490e
Lisa <mclisa00@gmail.com>
2023-02-17 13:48:40 +0000

    Manufacturer method test. Added test image

--- tests/manufacturer_test.py
+++ tests/manufacturer_test.py
@@ -2,9 +2,11 @@ import unittest
 from models.manufacturer import Manufacturer
 from datetime import datetime as dt
 
+from werkzeug.datastructures import FileStorage
+
 class TestManufacturer(unittest.TestCase):
     def setUp(self):
-        self.manufacturer = Manufacturer("Caron", dt(2023, 1, 12), 10000, "caron.jpeg")
+        self.manufacturer = Manufacturer("Caron", dt(2023, 1, 12), 10050, "caron.jpeg")
 
     def test_mfr_has_name(self):
         self.assertEqual(self.manufacturer.name, "Caron")
@@ -13,7 +15,10 @@ class TestManufacturer(unittest.TestCase):
         self.assertEqual(self.manufacturer.last_payment_date, dt(2023, 1, 12))
     
     def test_mfr_has_balance_due(self):
-        self.assertEqual(self.manufacturer.balance_due, 10000)
+        self.assertEqual(self.manufacturer.balance_due, 10050)
 
     def test_mfr_has_logo(self):
         self.assertEqual(self.manufacturer.logo, "caron.jpeg")
+
+    def test_mfr_format_currency(self):
+        self.assertEqual(self.manufacturer.format_currency_balance(), "£100.50")
