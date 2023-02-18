f0028692fa2f398a1aec350404eb7b216b75e3e3
Lisa <mclisa00@gmail.com>
2023-02-18 15:55:24 +0000

    Route and template to add manufacturer

--- controllers/manufacturers_controller.py
+++ controllers/manufacturers_controller.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from repositories import manufacturer_repository
+from models.manufacturer import Manufacturer
+from datetime import datetime as dt
 
 manufacturers_blueprint = Blueprint(
     "manufacturers", __name__, url_prefix="/manufacturers"
@@ -16,3 +18,17 @@ def manufacturers():
 def detail(id):
     manufacturer = manufacturer_repository.select(id)
     return render_template("/manufacturers/detail.html", manufacturer=manufacturer)
+
+
+@manufacturers_blueprint.route("/new", methods=["GET", "POST"])
+def create_manufacturer():
+    if request.method == "POST":
+        name = request.form["name"]
+        balance_due = request.form["balance_due"]
+
+        last_payment_date = request.form["last_payment_date"]
+        year, month, day = map(int, last_payment_date.split("-"))
+
+        new_manufacturer = Manufacturer(name, dt(year, month, day), balance_due)
+        manufacturer_repository.save(new_manufacturer)
+    return render_template("/manufacturers/new.html")
