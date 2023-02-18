2244c56c9ed19546ca367446a1d4dfa210e0accc
Lisa <mclisa00@gmail.com>
2023-02-18 12:13:00 +0000

    Landing page

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from controllers.manufacturers_controller import manufacturers_blueprint
 
 app = Flask(__name__)
+app.register_blueprint(manufacturers_blueprint)
 
-from controllers import controller
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
