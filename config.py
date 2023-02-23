571122e168b5309e6591215d8aec7fb99e05d24b
Lisa <mclisa00@gmail.com>
2023-02-23 12:22:23 +0000

    Changed yarn weight to tuple

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ manufacturer_fields = "name, last_payment_date, balance_due"
 
 yarn_table_name = "yarns"
 yarn_fields = "name, manufacturer_id, yarn_weight, ball_weight_grams, length_metres, needle_size_mm, fibre_type, buy_cost, sell_price, image"
-yarn_weights = ["2ply", "4ply", "DK", "Aran", "Chunky", "Super Chunky"]
+yarn_weights = ("2ply", "4ply", "DK", "Aran", "Chunky", "Super Chunky")
 
 colours_table_name = "colours"
 colours_fields = "name, hex_code, stock_quantity, yarn_id"
